2114cfd96a6b2a80c0f3ddf9615c8973b908397f
alisonsilvabt <101824638+alisonsilvabt@users.noreply.github.com>
2022-10-18 14:56:53 -0300

    Implement markers

---
+++ locations.ts
@@ -0,0 +1,18 @@
+export const locations_default = [
+    {
+        title: 'Instituto Federal de Alagoas',
+        description: 'Baterias',
+        coordinates: {
+            latitude: -9.70154848609437,
+            longitude: -36.687878610510936,    
+        },
+    },
+    {
+        title: 'Magazine Luiza',
+        description: 'Lâmpadas',
+        coordinates: {
+            latitude: -9.752138024493373, 
+            longitude: -36.65898066020981,
+        },
+    }
+];
