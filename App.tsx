725f9f851a49bc6b94b5f007725fa440ac8543a6
alisonsilvabt <101824638+alisonsilvabt@users.noreply.github.com>
2022-10-18 01:26:17 -0300

    Tela inicial contendo mapa

--- App.tsx
+++ App.tsx
@@ -1,28 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { ThemeProvider } from 'styled-components'
-import {theme} from './src/theme/light'
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+
+  const [origin, setOrigin] = useState(null)
+  const [destination, setDestination] = useState(null)
+
+  useEffect(() => {
+    (async function() {
+      const {status} = await Location.requestForegroundPermissionsAsync();
+      if (status == 'granted') {
+        let location = await Location.getCurrentPositionAsync({});
+        console.log(location)
+        setOrigin({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        })
+        //setLocation(location);
+      }else{
+        throw new Error("Location permission not granted")
+      }
+    }) ();
+  }, [])
+
   return (
-    <>
-    <ThemeProvider theme={theme}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+          style={styles.map}
+          initialRegion={origin}
+          showsUserLocation = {true}
+          loadingEnabled = {true}
+        >
+      </MapView>
     </View>
-    </ThemeProvider>
-    </>
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: theme.colors.primary,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});*/
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
