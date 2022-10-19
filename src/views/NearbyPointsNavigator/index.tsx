53f4e4bed7d25693e684acc5624d49d11bf03693
Ericlefyson Silva <65918083+ericlefyson@users.noreply.github.com>
2022-10-19 09:22:05 -0300

    Iniciado Lista

--- src/views/NearbyPointsNavigator/index.tsx
+++ src/views/NearbyPointsNavigator/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 import { Text, View } from 'react-native';
+import * as S from './style'
+import { List } from '../../components'
+import imageURL from '../../../assets/logo.png'
+import { useLocations } from '../../hooks/Locations'
+
+
 
 export const NearbyPoints = () => {
-    return (
-      <View>
-        <Text>New Post!</Text>
-      </View>
-    );
-  }
+  const { locations } = useLocations()
+
+  return (
+    <S.Container>
+      <S.LogoContainer>
+        <S.Logo source={imageURL} />
+      </S.LogoContainer>
+      <S.InputsColumn>
+        <List data={locations}/>
+      </S.InputsColumn>
+    </S.Container>
+  );
+}
