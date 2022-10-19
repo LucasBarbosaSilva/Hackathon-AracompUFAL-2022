b1bc9877b89ee8b1c2c2bc3ec773246aaaff4687
alisonsilvabt <101824638+alisonsilvabt@users.noreply.github.com>
2022-10-19 11:16:20 -0300

    modify files

--- src/views/RegisterNavigator/index.tsx
+++ src/views/RegisterNavigator/index.tsx
@@ -1,17 +1,88 @@
 import React from 'react'
-import * as S from './style'
+import { View, Text, StyleSheet, useWindowDimensions } from 'react-native'
+//import {Text} from '../../components'
+import MaterialsIcon from 'react-native-vector-icons/MaterialIcons';
+import { Fumi, Madoka } from 'react-native-textinput-effects';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import Checkbox from '@mui/material/Checkbox';
 import imageURL from '../../../assets/logo.png'
-
+import * as S from './style'
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export const RegisterPointScreen = () => {
+    const window = useWindowDimensions()
     return (
-        <S.Container>
-            <S.LogoContainer>
-             <S.Logo source={imageURL}/>
-            </S.LogoContainer>
-            <S.InputsColumn>
-                {/* <Input/> */}
-            </S.InputsColumn>
-        </S.Container>)
+        <View style = {{paddingHorizontal: 20, height:window.height, justifyContent:'center'}}>
+        <S.Logo source={imageURL} />
+        <Fumi
+            style={styles.container}
+            label={'Nome do estabelecimento'}
+            // this is used as active and passive border color
+            inputPadding={16}
+
+            iconClass={FontAwesomeIcon}
+            iconName={'university'}
+            iconColor={'#f95a25'}
+            iconSize={20}
+            iconWidth={40}
+            inputPadding={16}
+        />
+        <Fumi
+            style={styles.container}
+            label={'CEP'}
+            // this is used as active and passive border color
+            iconClass={FontAwesomeIcon}
+            iconName={'university'}
+            iconColor={'#f95a25'}
+            iconSize={20}
+            iconWidth={40}
+            inputPadding={16}
+        />
+        <Fumi
+            style={styles.container}
+            label={'Rua'}
+            // this is used as active and passive border color
+            iconClass={FontAwesomeIcon}
+            iconName={'university'}
+            iconColor={'#f95a25'}
+            iconSize={20}
+            iconWidth={40}
+            inputPadding={16}
+        />
+        <Fumi
+            style={styles.container}
+            label={'Numero'}
+            // this is used as active and passive border color
+            iconClass={FontAwesomeIcon}
+            iconName={'university'}
+            iconColor={'#f95a25'}
+            iconSize={20}
+            iconWidth={40}
+            inputPadding={16}
+        />
+        <Fumi
+            style={styles.container}
+            label={'Checkboxes'}
+            // this is used as active and passive border color
+            iconClass={FontAwesomeIcon}
+            iconName={'university'}
+            iconColor={'#f95a25'}
+            iconSize={20}
+            iconWidth={40}
+            inputPadding={16}
+        />
+        </View>
+    )
 }
 
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding:15,
+      marginBottom:10,
+      borderColor: '#A5F1D9',
+      borderWidth: 1,
+      borderRadius: 10
+    }
+});
--- src/views/RegisterNavigator/style.ts
+++ src/views/RegisterNavigator/style.ts
@@ -1,23 +1,13 @@
 import styled from 'styled-components/native'
-import * as imageURL from '../../../assets/logo.png'
-import {Image} from 'react-native'
 
-interface themeProp {
-    theme: any
-}
-
-export const Container = styled.View<themeProp>`
+export const Container = styled.View`
 flex: 1;
 justify-content: center;
 align-items: center;
-background-color: ${({ theme }) => theme.colors.bg}
 `
-
-export const LogoContainer = styled.View``
 
 export const Logo = styled(Image)`
     width: 168px;
     height: 32px;
-
 `
-export const InputsColumn = styled.View``
+
